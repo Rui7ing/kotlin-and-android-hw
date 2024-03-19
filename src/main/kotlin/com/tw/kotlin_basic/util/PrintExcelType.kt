8c059ee19b3b58d8f7b6910edd4d69677d5535b7
bomu <ruiqing.piao@thoughtworks.com>
2024-03-19 14:07:39 +0800

    feat get character using int

--- src/main/kotlin/com/tw/kotlin_basic/util/PrintExcelType.kt
+++ src/main/kotlin/com/tw/kotlin_basic/util/PrintExcelType.kt
@@ -2,11 +2,17 @@ package com.tw.kotlin_basic.util
 
 class PrintExcelType {
 
-    fun getCharacter(number: Int): String {
+    fun getCharacter(number: Long): String {
+        if (number <= 0) {
+            throw IllegalArgumentException("number must be greater than 0")
+        }
+        if (number >= 26*26*26 + 26*26 + 26) {
+            throw IllegalArgumentException("character must be smaller than 'ZZZ'")
+        }
         var result = ""
         var tmp = number - 1
         while (tmp >= 0) {
-            result = 'A'.plus(tmp % 26) + result
+            result = 'A'.plus((tmp % 26).toInt()) + result
             tmp = tmp/26 - 1
         }
         return result
--- src/test/kotlin/com/tw/kotlin_basic/util/PrintExcelTypeTest.kt
+++ src/test/kotlin/com/tw/kotlin_basic/util/PrintExcelTypeTest.kt
@@ -14,4 +14,18 @@ class PrintExcelTypeTest {
         Assertions.assertEquals("AA", printExcelType.getCharacter(27))
         Assertions.assertEquals("AB", printExcelType.getCharacter(28))
     }
+
+    @Test
+    fun throwExceptionIfNumberIsZero() {
+        Assertions.assertThrows(IllegalArgumentException::class.java) {
+            printExcelType.getCharacter(0)
+        }
+    }
+
+    @Test
+    fun throwExceptionIfCharacterMoreThanZZZ() {
+        Assertions.assertThrows(IllegalArgumentException::class.java) {
+            printExcelType.getCharacter(26*26*26+26*26+26)
+        }
+    }
 }
